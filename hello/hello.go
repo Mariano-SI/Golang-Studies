52c0204e6dd0f3a5de8e541cb7bc788c3ca84577
Mariano-SI <Mariano.silva@ufvjm.edu.br>
2024-11-15 12:45:35 -0300

    começando menu

--- hello/hello.go
+++ hello/hello.go
@@ -1,14 +1,21 @@
 package main
 
 import "fmt"
-import "reflect"
+
 
 func main() {
 	name := "Mariano"
 	version :=  1.1
-	age := 23
-	fmt.Println("Olá, sr.", name, " sua idade é ", age)
+
+	fmt.Println("Olá, sr.", name)
 	fmt.Println("Este programa está na versão", version)
 
-	fmt.Println("O tipo da variável name é", reflect.TypeOf(version))
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var command int
+	fmt.Scan(&command)
+
+	fmt.Println("O comando escolhido foi", command)
 }
