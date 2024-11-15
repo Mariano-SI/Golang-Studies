63699dfa964eeedc5563432a4a00b156d3105775
Mariano-SI <Mariano.silva@ufvjm.edu.br>
2024-11-15 12:35:02 -0300

    inferencia de tipos

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,14 @@
 package main
 
 import "fmt"
+import "reflect"
 
 func main() {
-	fmt.Println("Hello, World! aaaaaaaa")
+	name := "Mariano"
+	version :=  1.1
+	age := 23
+	fmt.Println("Olá, sr.", name, " sua idade é ", age)
+	fmt.Println("Este programa está na versão", version)
+
+	fmt.Println("O tipo da variável name é", reflect.TypeOf(version))
 }
