3ca3720ffbbf0b9d3bbafbfe91da3667e051d17d
Mariano-SI <Mariano.silva@ufvjm.edu.br>
2024-11-15 13:16:15 -0300

    introducao as funcoes

--- hello/hello.go
+++ hello/hello.go
@@ -1,34 +1,15 @@
 package main
 
 import "fmt"
+import "os"
 
 
 func main() {
-	name := "Mariano"
-	version :=  1.1
+	showIntroduction()
 
-	fmt.Println("Olá, sr.", name)
-	fmt.Println("Este programa está na versão", version)
+	showMenu()
 
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("0 - Sair do Programa")
-
-	var command int
-	fmt.Scan(&command)
-
-	fmt.Println("O comando escolhido foi", command)
-
-
-	if command == 1 {
-		fmt.Println("Monitorando...")
-	} else if command == 2 {
-		fmt.Println("Exibindo Logs...")
-	} else if command == 0 {
-		fmt.Println("Saindo do Programa...")
-	} else {
-		fmt.Println("Não conheço este comando")
-	}
+	command := readCommand()
 	
 	switch command {
 		case 1:
@@ -37,8 +18,31 @@ func main() {
 			fmt.Println("Exibindo Logs...")
 		case 0:
 			fmt.Println("Saindo do Programa...")
+			os.Exit(0)
 		default:
 			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
 	}
 	
+}
+
+func showIntroduction() {
+	name := "Mariano"
+	version :=  1.1
+
+	fmt.Println("Olá, sr.", name)
+	fmt.Println("Este programa está na versão", version)
+}
+
+func showMenu(){
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+}
+
+func readCommand()int{
+	var readComannd int
+	fmt.Scan(&readComannd)
+	fmt.Println("O comando escolhido foi", readComannd)
+	return readComannd
 }
